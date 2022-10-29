b5f04a552d5ed759361df53931cd5d17bf3300d1
nimeshgithub15 <116950933+nimeshgithub15@users.noreply.github.com>
2022-10-29 16:27:26 +0530

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+ int main()
+ 
+ {
+
+float radius;
+
+float area ;
+
+cout<<"Enter radius of a circle";
+
+cin>>radius;
+
+area= 3.14*radius*radius;
+
+cout<<" Area of a circle is"<<area<<endl;
+
+return 0;
+
+      
+ }
